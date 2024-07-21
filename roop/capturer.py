ad78aaad38f1605bbc936e0acb9e6568a5623fbb
C0untFloyd <131583554+C0untFloyd@users.noreply.github.com>
2024-07-21 16:50:45 +0200

    Videohandle is remembered after opening, speeding up seeking/swapping videos

--- roop/capturer.py
+++ roop/capturer.py
@@ -4,6 +4,10 @@ import numpy as np
 
 from roop.typing import Frame
 
+current_video_path = None
+current_frame_total = 0
+current_capture = None
+
 def get_image_frame(filename: str):
     try:
         return cv2.imdecode(np.fromfile(filename, dtype=np.uint8), cv2.IMREAD_COLOR)
@@ -13,11 +17,18 @@ def get_image_frame(filename: str):
 
     
 def get_video_frame(video_path: str, frame_number: int = 0) -> Optional[Frame]:
-    capture = cv2.VideoCapture(video_path)
-    frame_total = capture.get(cv2.CAP_PROP_FRAME_COUNT)
-    capture.set(cv2.CAP_PROP_POS_FRAMES, min(frame_total, frame_number - 1))
-    has_frame, frame = capture.read()
-    capture.release()
+    global current_video_path, current_capture, current_frame_total
+
+    if video_path != current_video_path:
+        if current_capture is not None:
+            current_capture.release()
+
+        current_capture = cv2.VideoCapture(video_path)
+        current_video_path = video_path
+        current_frame_total = current_capture.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    current_capture.set(cv2.CAP_PROP_POS_FRAMES, min(current_frame_total, frame_number - 1))
+    has_frame, frame = current_capture.read()
     if has_frame:
         return frame
     return None
